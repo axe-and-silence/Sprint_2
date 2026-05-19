84d99077fc4f4426c27b08040c22e718d1b977ba
axe-and-silence <111899124+axe-and-silence@users.noreply.github.com>
2026-05-19 10:58:20 +0300

    Update task_4.py решение конфликтов

--- task_4.py
+++ task_4.py
@@ -1,61 +1,39 @@
-#С помощью переменной hourly_payment установи почасовой уровень оплаты, равный 400.
-#Проинициализируй атрибуты name, hours, rest_days, email через конструктор.
-#Добавь метод класса get_hours(). Если значение hours неизвестно, метод рассчитывает часы, исходя из количества выходных — rest_days. Формула для этого случая такая: (7 - rest_days) * 8.
-#Добавь метод класса get_email(). Если значение email неизвестно, метод генерирует его так: f"{name}@email.com".
-#Добавь метод класса set_hourly_payment(). Он меняет значение переменной hourly_payment.
-#Добавь метод расчёта заработной платы salary(). Формула расчёта такая: hours * hourly_payment.
-
-
 class EmployeeSalary:
-
     hourly_payment = 400
-
-   
-
-    def __init__(self, name, hours=0, rest_days=0, email=None):
-
+    
+    def __init__(self, name, hours=None, rest_days=0, email=None):
         self.name = name
-
         self.hours = hours
-
         self.rest_days = rest_days
-
         self.email = email
-
-   
-
+    
+class EmployeeSalary:
+    hourly_payment = 400
+    
+    def __init__(self, name, hours=None, rest_days=0, email=None):
+        self.name = name
+        self.hours = hours
+        self.rest_days = rest_days
+        self.email = email
+    
     @classmethod
-
-    def get_hours(cls, name, hours=None, rest_days=0, email=None):
-
-        if hours is None and rest_days > 0:
-
-            hours = (7 - rest_days) * 8
-
-        return cls(name, hours, rest_days, email)
-
-   
-
+    def get_hours(cls, quantity):
+        if quantity.hours is None:
+            return (7 - quantity.rest_days) * 8
+        else:
+            return quantity.hours
+            
     @classmethod
-
-    def get_email(cls, name, hours=None, rest_days=0, email=None):
-
-        if email is None:
-
-            email = f"{name}@email.com"
-
-        return cls(name, hours, rest_days, email)
-
-   
-
+    def get_email(cls, quantity):
+        if quantity.email is None:
+            return f"{quantity.name}@email.com" 
+        else:
+            return quantity.email
+    Collapse commentComment on lines R19 to R32AlexKitIt commented on May 16, 2026 AlexKitIton May 16, 2026More actionsНужно исправить: эти методы принимают класс и все атрибуты и если конкретный None - выполняют его вычисление и вернут класс со всеми атрибутамиReactWrite a replyResolve comment
     @classmethod
-
-    def set_hourly_payment(cls, new_payment):
-
-        cls.hourly_payment = new_payment
-
-   
+    def set_hourly_payment(cls, new_value):
+        cls.hourly_payment = new_value
 
     def salary(self):
-
-        return self.hours * self.hourly_payment
+        worked_hours = self.get_hours(self)
+        return worked_hours * self.hourly_payment
