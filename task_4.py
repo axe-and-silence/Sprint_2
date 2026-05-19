34ef501e20c0afb2c1bb874bce99cd31723b2e5a
axe-and-silence <111899124+axe-and-silence@users.noreply.github.com>
2026-05-19 11:05:22 +0300

    Update task_4.py решение конфликтов

--- task_4.py
+++ task_4.py
@@ -7,33 +7,21 @@ class EmployeeSalary:
         self.rest_days = rest_days
         self.email = email
     
-class EmployeeSalary:
-    hourly_payment = 400
-    
-    def __init__(self, name, hours=None, rest_days=0, email=None):
-        self.name = name
-        self.hours = hours
-        self.rest_days = rest_days
-        self.email = email
+       @classmethod
+    def get_hours(cls, name, hours=None, rest_days=0, email=None):
+        if hours is None and rest_days > 0:
+            hours = (7 - rest_days) * 8
+        return cls(name, hours, rest_days, email)
     
     @classmethod
-    def get_hours(cls, quantity):
-        if quantity.hours is None:
-            return (7 - quantity.rest_days) * 8
-        else:
-            return quantity.hours
-            
+    def get_email(cls, name, hours=None, rest_days=0, email=None):
+        if email is None:
+            email = f"{name}@email.com"
+        return cls(name, hours, rest_days, email)
+    
     @classmethod
-    def get_email(cls, quantity):
-        if quantity.email is None:
-            return f"{quantity.name}@email.com" 
-        else:
-            return quantity.email
-    Collapse commentComment on lines R19 to R32AlexKitIt commented on May 16, 2026 AlexKitIton May 16, 2026More actionsНужно исправить: эти методы принимают класс и все атрибуты и если конкретный None - выполняют его вычисление и вернут класс со всеми атрибутамиReactWrite a replyResolve comment
-    @classmethod
-    def set_hourly_payment(cls, new_value):
-        cls.hourly_payment = new_value
-
+    def set_hourly_payment(cls, new_payment):
+        cls.hourly_payment = new_payment
+    
     def salary(self):
-        worked_hours = self.get_hours(self)
-        return worked_hours * self.hourly_payment
+        return self.hours * self.hourly_payment
